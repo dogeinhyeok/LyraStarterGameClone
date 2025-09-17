6ec267b0213398a62350f15ed7b896f1f4b8a377
dogeinhyeok <devinhyeok@gmail.com>
2025-09-17 18:11:45 +0900

    feat: Cosmetic 파트 1 시작

---
+++ Source/LyraStarterGameClone/Cosmetics/LyraControllerComponent_CharacterParts.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ControllerComponent.h"
+#include "LyraControllerComponent_CharacterParts.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class LYRASTARTERGAMECLONE_API ULyraControllerComponent_CharacterParts : public UControllerComponent
+{
+	GENERATED_BODY()
+};
