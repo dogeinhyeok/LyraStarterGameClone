9d8c611907f97a0b527440d8ae7d9326b23da409
dogeinhyeok <devinhyeok@gmail.com>
2025-09-19 20:07:57 +0900

    feat: Core Redirect 파트 완강

---
+++ Source/LyraStarterGameClone/Animation/LyraAnimInstance.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "LyraAnimInstance.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class LYRASTARTERGAMECLONE_API ULyraAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadOnly, Category = "Character State Data")
+	float GroundDistance = -1.0f;
+};
